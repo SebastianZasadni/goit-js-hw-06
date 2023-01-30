0560d4bcad1a609c9d6410cfbe695f3beb79a8de
SebastianZasadni <emblelele@gmail.com>
2023-01-30 18:53:51 +0100

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const newIngredients = document.querySelector('ul');
+const createList = ingredients.map(ingredient => {
+  const listElement = document.createElement("li");
+  listElement.textContent = ingredient;
+  listElement.classList.add("item");
+  newIngredients.append(listElement);
+ });
