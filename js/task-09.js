9e16f9dd93f58d933ea020624f39c0b51a5fb451
SebastianZasadni <emblelele@gmail.com>
2023-02-02 20:53:04 +0100

    feat task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const changeButton = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+const changeColor = () => {
+  const bgColor = getRandomHexColor();
+  spanColor.innerHTML = bgColor;
+  body.style.backgroundColor = bgColor;
+};
+
+changeButton.addEventListener("click", changeColor);
