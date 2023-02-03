0ff7aa90bfe116841ab7313c412be7503425bada
SebastianZasadni <emblelele@gmail.com>
2023-02-03 22:19:34 +0100

    fix task-06

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,13 @@ const validationInput = document.querySelector('[data-length="6"]');
 
 validationInput.addEventListener("input", () => {
     if (validationInput.value.length === 6){
-              document.activeElement.setAttribute("class", "valid"); 
+        document.activeElement.setAttribute("class", "valid"); 
                              }
     else{
     validationInput.setAttribute("class", "invalid");
         }
+        if (validationInput.value.length === 0){
+            validationInput.setAttribute("class", "normal")
+            }
 }); 
+
