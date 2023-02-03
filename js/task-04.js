1f359feaadb2dddcbc5818d665709b64e11dfce4
SebastianZasadni <emblelele@gmail.com>
2023-02-03 11:09:36 +0100

    fix task-04

--- js/task-04.js
+++ js/task-04.js
@@ -2,13 +2,22 @@ let counterValue = 0;
 const decrement = document.querySelector('[data-action="decrement"]');
 const increment = document.querySelector('[data-action="increment"]');
 const value = document.querySelector('#value');
-
+decrement.setAttribute("class", "disabled"); 
 const lower = () => {
+    if (value.textContent < 1){
+        decrement.setAttribute("class", "disabled"); 
+            }
+    else{
     counterValue -= 1;
     value.innerHTML = counterValue;
+    if (value.textContent < 1){
+        decrement.setAttribute("class", "disabled"); 
+            }
+    }
 }
 const increase = () => {
-counterValue += 1;
+    decrement.setAttribute("class", null); 
+    counterValue += 1;
 value.innerHTML = counterValue;
 }
 
